b368d469edf46cccbc94b0dd655a9614ba2ae368
anujkashyap <anujkashyap1330@gmail.com>
2024-08-09 22:42:43 +0530

    practice

---
+++ practice/practice3.js
@@ -0,0 +1,11 @@
+// what is value of variable in each line of ConvolverNode
+let num = 5;
+// here value of num is 5
+let newnum = num++;
+// here newnum value is 5 which is getting from num
+// here num value is 6 due to increment
+newnum = ++num;
+// here num become 7 and newnum is also 7, newnum is taking value of num;
+console.log(num);
+console.log(newnum);
+//here the value if 7  from both
